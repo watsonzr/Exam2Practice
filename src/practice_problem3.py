e998c2ec1f1b3d781ed789e2c82eb5a6a3ee2561
watsonzr <watsonzr@rose-hulman.edu>
2018-04-02 20:04:14 -0400

    Final Version of practice_problem3

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -291,6 +291,7 @@ def practice_problem3b(sequence):
             return True
     return False
 
+
 def run_test_practice_problem3c():
     """ Tests the    practice_problem3c    function. """
     # ------------------------------------------------------------------
@@ -365,13 +366,19 @@ def practice_problem3c(sequence):
       :type: sequence: list    or tuple or string
     """
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes.
     ####################################################################
+
+    list_of_zero_index = []
+    for i in range(len(sequence)):
+        if sequence[i] == 0:
+            list_of_zero_index = list_of_zero_index + [i]
+    return list_of_zero_index
 
 
 def run_test_practice_problem3d():
@@ -447,7 +454,7 @@ def practice_problem3d(sequence):
       :type: sequence: list    or tuple or string
     """
     ####################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
@@ -455,22 +462,32 @@ def practice_problem3d(sequence):
     #    TIME ESTIMATE:   8 minutes for each part of this problem.
     ####################################################################
 
+    # for i in range(len(sequence)):
+    #     if sequence[i] == 0:
+    #         return i
+    # return -1
 
     ####################################################################
-    # TODO: 6. Just ABOVE this TODO, you should have implemented
+    # DONE: 6. Just ABOVE this TO DO, you should have implemented
     #     a solution for the   practice_problem3d   function.
     #     Here, put ANOTHER solution, as follows:
     #
-    #       -- Your FIRST solution (ABOVE this TODO)
+    #       -- Your FIRST solution (ABOVE this TO DO)
     #            should be a solution that IGNORES
     #              practice_problem3c (the previous problem).
     #
-    #       -- Your SECOND solution (BELOW this TODO)
+    #       -- Your SECOND solution (BELOW this TO DO)
     #            should be a solution that USES (calls)
     #              practice_problem3c.
     #
     #          This solution should *** HAVE NO LOOP (no FOR). ***
     ####################################################################
+
+    list_of_zero_index = practice_problem3c(sequence)
+    if len(list_of_zero_index) == 0:
+        return -1
+    else:
+        return list_of_zero_index[0]
 
 
 def run_test_practice_problem3e():
@@ -533,13 +550,19 @@ def practice_problem3e(sequence):
       :type sequence: list(float)    or tuple(float)
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes.
     ####################################################################
+
+    total = 0
+    for i in range(len(sequence)):
+        if i % 2 == 0:
+            total = total + sequence[i]
+    return total
 
 
 # ----------------------------------------------------------------------
